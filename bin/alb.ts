2410bb7781dc1011f2b6b87b26bbbacf92882f8f
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-03 17:46:39 -0500

    Initialize a cdk app with cdk init app

---
+++ bin/alb.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { AlbStack } from '../lib/alb-stack';
+
+const app = new cdk.App();
+new AlbStack(app, 'AlbStack');
