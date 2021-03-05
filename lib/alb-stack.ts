7ade7eb6d3849aaa1940970ed9ebf41f9e127bae
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-04 19:48:22 -0500

    Remove the ALB
    I have completed the experiment that I started this branch for,
    analyzing ALB access logs in Athena, so I am removing the costly
    resource, the ALB.
    
    The Athena work was completed in:
    
    https://github.com/douglasnaphas/athena-alb-access-logs-1

--- lib/alb-stack.ts
+++ lib/alb-stack.ts
@@ -7,34 +7,34 @@ export class AlbStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const accessLogBucket = new s3.Bucket(this, "albAccessLogBucket");
-    const vpc = new ec2.Vpc(this, "VPC");
-    const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
-      vpc,
-      internetFacing: true,
-    });
-    lb.logAccessLogs(accessLogBucket);
-    const listener = lb.addListener("HttpsListener", {
-      port: 80,
-      open: true,
-    });
-    listener.addAction("DefaultAction", {
-      action: elbv2.ListenerAction.fixedResponse(200, {
-        contentType: elbv2.ContentType.TEXT_PLAIN,
-        messageBody: "OK-default-response",
-      }),
-    });
-    listener.addAction("OkAction", {
-      priority: 10,
-      conditions: [elbv2.ListenerCondition.pathPatterns(["/ok"])],
-      action: elbv2.ListenerAction.fixedResponse(200, {
-        contentType: elbv2.ContentType.TEXT_PLAIN,
-        messageBody: "OK-ok-response",
-      }),
-    });
+    // const vpc = new ec2.Vpc(this, "VPC");
+    // const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
+    //   vpc,
+    //   internetFacing: true,
+    // });
+    // lb.logAccessLogs(accessLogBucket);
+    // const listener = lb.addListener("HttpsListener", {
+    //   port: 80,
+    //   open: true,
+    // });
+    // listener.addAction("DefaultAction", {
+    //   action: elbv2.ListenerAction.fixedResponse(200, {
+    //     contentType: elbv2.ContentType.TEXT_PLAIN,
+    //     messageBody: "OK-default-response",
+    //   }),
+    // });
+    // listener.addAction("OkAction", {
+    //   priority: 10,
+    //   conditions: [elbv2.ListenerCondition.pathPatterns(["/ok"])],
+    //   action: elbv2.ListenerAction.fixedResponse(200, {
+    //     contentType: elbv2.ContentType.TEXT_PLAIN,
+    //     messageBody: "OK-ok-response",
+    //   }),
+    // });
 
-    new cdk.CfnOutput(this, "loadBalancerDnsName", {
-      value: lb.loadBalancerDnsName,
-    });
+    // new cdk.CfnOutput(this, "loadBalancerDnsName", {
+    //   value: lb.loadBalancerDnsName,
+    // });
     new cdk.CfnOutput(this, "logBucketName", {
       value: accessLogBucket.bucketName,
     });
